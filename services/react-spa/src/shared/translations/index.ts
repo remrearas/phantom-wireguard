a7d76fc60d36c4c2599d899ed88c609bb8a9305a
Rıza Emre ARAS <r.emrearas@proton.me>
2026-03-09 04:36:32 +0300

    Refactor: Split translations into per-locale files and set English as default language

--- services/react-spa/src/shared/translations/index.ts
+++ services/react-spa/src/shared/translations/index.ts
@@ -1,14 +1,11 @@
-import translationsData from './translations.json';
+import en from './en/index.json';
+import tr from './tr/index.json';
 
-export type Locale = 'tr' | 'en';
+export type Locale = 'en' | 'tr';
 
-export const DEFAULT_LOCALE: Locale = translationsData._default as Locale;
+export const DEFAULT_LOCALE: Locale = 'en';
 
-export const translations = translationsData as {
-  _default: Locale;
-  tr: typeof translationsData.tr;
-  en: typeof translationsData.en;
-};
+export const translations: Record<Locale, typeof en> = { en, tr };
 
 export const translate = (locale: Locale) => {
   return translations[locale] || translations[DEFAULT_LOCALE];
